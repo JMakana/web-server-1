1e664f6f88553c8cd81a701cdcbe7c9ede12832e
mcchaves <eleceya.2@gmail.com>
2018-10-02 18:47:56 +0200

    Partial views

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
+const path = require ('path')
 const { consoleLogMiddleware, fileLogMiddleware } = require('./middleware')
 
-app.use(consoleLogMiddleware)
+const hbs = require('hbs')
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+app.set('view engine', 'hbs') // clave valor
 
+app.use(consoleLogMiddleware)
 app.use(fileLogMiddleware)
+
+const staticRoute =path.join(__dirname, 'public')
+app.use(express.static(staticRoute))
 
 app.get('/', (req, res) => {
   res.send('Hola Juanito')
 })
 
 app.get('/contactar', (req, res) => {
-  res.send({ nombre: 'pepe', correo: 'pepe@pepe.com' })
+  res.render('contactar.hbs', {
+    pageTitle: 'Contactar',
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.listen(3000, () => {
