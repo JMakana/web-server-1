79b5374a9ec51f506db6357429bd803229dca07c
juanda <juandacorreo@gmail.com>
2018-10-02 17:39:18 +0200

    añadidos middlewares

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
-app.get(
-  '/',
-  (req, res) => {
-    res.send('Hola Juanito')
-  }
-)
+const { consoleLogMiddleware, fileLogMiddleware } = require('./middleware')
+
+app.use(consoleLogMiddleware)
+
+app.use(fileLogMiddleware)
+
+app.get('/', (req, res) => {
+  res.send('Hola Juanito')
+})
+
+app.get('/contactar', (req, res) => {
+  res.send({ nombre: 'pepe', correo: 'pepe@pepe.com' })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
